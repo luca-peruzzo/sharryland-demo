c1cd53352ed020f02467144a2caeeef71a0aa62a
Luca Peruzzo <luca.peruzzo@sharry.land>
2024-08-24 11:02:13 +0200

    up

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,23 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { BeerService } from './services/beer.service';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-    imports: [
-        RouterTestingModule,
-        AppComponent
-    ],
-}).compileComponents();
+      imports: [
+        provideHttpClient(withInterceptorsFromDi()),
+        provideHttpClientTesting(),
+        AppComponent,
+      ],
+      providers:[BeerService]
+    }).compileComponents();
   });
 
   it('should create the app', () => {
@@ -28,6 +36,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('sharryland-demo app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain(
+      'sharryland-demo app is running!'
+    );
   });
 });
--- src/app/services/beer.service.spec.ts
+++ src/app/services/beer.service.spec.ts
@@ -1,12 +1,22 @@
 import { TestBed } from '@angular/core/testing';
 
 import { BeerService } from './beer.service';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('BeerService', () => {
   let service: BeerService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(withInterceptorsFromDi()),
+        provideHttpClientTesting(),
+      ],
+    });
     service = TestBed.inject(BeerService);
   });
 
